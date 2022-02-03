05cdacfd1c8490e943d5763d7c2a2c31c1a5c90f
kaanbozdogan <kaanbzdgn@gmail.com>
2022-02-03 14:56:11 +0300

    .

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
-#include"vec.cpp"
-#include"iter.cpp"
-
-using namespace std;
+#include"vec.h"
+#include<vector>
 
 
 
@@ -9,125 +7,121 @@ int main()
 {
 	Vec<int> v1, v2;
 
-	cout << "-> v1 = Vec();" << endl;
+	std::cout << "-> v1 = Vec();" << std::endl;
 	v1 = Vec<int>();
-	cout << v1;
+	std::cout << v1;
 
-	cout << endl << "-> v1 = Vec(12, 3);" << endl;
+	std::cout << std::endl << "-> v1 = Vec(12, 3);" << std::endl;
 	v1 = Vec(12, 3);
-	cout << v1;
-/*
-	cout << endl << "-> " << endl;
-	v2 = Vec(v1.begin(), v1.end());
-	cout << v2;
-*/
-	cout << endl << "-> v2 = Vec(v1);" << endl;
+	std::cout << v1;
+
+	std::cout << std::endl << "-> v2 = Vec(v1);" << std::endl;
 	v2 = Vec(v1);
-	cout << v2;
+	std::cout << v2;
 
-	cout << endl << "-> v1 = Vec({11,22,33,44});" << endl;
+	std::cout << std::endl << "-> v1 = Vec({11,22,33,44});" << std::endl;
 	v1 = Vec({11,22,33,44});
-	cout << v1;
+	std::cout << v1;
 
-	cout << endl << "-> v2 = Vec(move(v1));" << endl;
-	v2 = Vec(move(v1));
-	cout << v2;
+	std::cout << std::endl << "-> v2 = Vec(std::move(v1));" << std::endl;
+	v2 = Vec(std::move(v1));
+	std::cout << v2;
 
-	cout << endl << "-> int arr[4] = {1,2,3,4,5,6,7};" << endl << "-> v1 = Vec(&arr[0], &arr[7]);" << endl;
+	std::cout << std::endl << "-> int arr[4] = {1,2,3,4,5,6,7};" << std::endl << "-> v1 = Vec(&arr[0], &arr[7]);" << std::endl;
 	int arr[7] = {1,2,3,4,5,6,7};
 	v1 = Vec(&arr[0], &arr[7]);
-	cout << v1;
+	std::cout << v1;
 	
-	cout << endl << "-> v2 = v1;" << endl;
+	std::cout << std::endl << "-> v2 = v1;" << std::endl;
 	v2 = v1;
-	cout << v2;
+	std::cout << v2;
 
-	cout << endl << "-> v1.push_back(101);" << endl;
+	std::cout << std::endl << "-> v1.push_back(101);" << std::endl;
 	v1.push_back(101);
-	cout << v1;
+	std::cout << v1;
 
-	cout << endl << "-> v2 = move(v1);" << endl;
-	v2 = move(v1);
-	cout << v2;
+	std::cout << std::endl << "-> v2 = std::move(v1);" << std::endl;
+	v2 = std::move(v1);
+	std::cout << v2;
 
-	cout << endl << "-> v2.pop_back();" << endl;
+	std::cout << std::endl << "-> v2.pop_back();" << std::endl;
 	v2.pop_back();
-	cout << v2;
+	std::cout << v2;
 
-	cout << endl << "-> v2.reserve(8);" << endl;
+	std::cout << std::endl << "-> v2.reserve(8);" << std::endl;
 	v2.reserve(8);
-	cout << v2;
+	std::cout << v2;
 
-	cout << endl << "-> v2.reserve(12);" << endl;
+	std::cout << std::endl << "-> v2.reserve(12);" << std::endl;
 	v2.reserve(12);
-	cout << v2;
+	std::cout << v2;
 
-	cout << endl << "-> v2.shrink_to_fit();" << endl;
+	std::cout << std::endl << "-> v2.shrink_to_fit();" << std::endl;
 	v2.shrink_to_fit();
-	cout << v2;
+	std::cout << v2;
 
-	cout << endl << "-> v2.assign(5, 99);" << endl;
+	std::cout << std::endl << "-> v2.assign(5, 99);" << std::endl;
 	v2.assign(5, 99);
-	cout << v2;
+	std::cout << v2;
 		
-	cout << endl << "-> v2.clear();" << endl;
+	std::cout << std::endl << "-> v2.clear();" << std::endl;
 	v2.clear();
-	cout << v2;
+	std::cout << v2;
 
-	cout << endl << "-> v2.assign({5,4,3,2,1,0,-1});" << endl;
+	std::cout << std::endl << "-> v2.assign({5,4,3,2,1,0,-1});" << std::endl;
 	v2.assign({5,4,3,2,1,0,-1});
-	cout << v2;
+	std::cout << v2;
 
-	cout << endl << "-> v1.assign(&arr[0], &arr[7]);" << endl;
+	std::cout << std::endl << "-> v1.assign(&arr[0], &arr[7]);" << std::endl;
 	v1.assign(&arr[0], &arr[7]);
-	cout << v1;
+	std::cout << v1;
 	
-	cout << endl << "-> v2[3]: " << v2[3] << endl;
+	std::cout << std::endl << "-> v2[3]: " << v2[3] << std::endl;
 
-	cout << endl << "-> v1.swap(v2);" << endl;
+	std::cout << std::endl << "-> v1.swap(v2);" << std::endl;
 	v1.swap(v2);
-	cout << "v1: " << v1;
-	cout << "v2: " << v2;
+	std::cout << "v1: " << v1;
+	std::cout << "v2: " << v2;
 
-	cout << endl << "-> v1.resize(3);" << endl;
+	std::cout << std::endl << "-> v1.resize(3);" << std::endl;
 	v1.resize(3);
-	cout << v1;
+	std::cout << v1;
 
-	cout << endl << "-> v1.resize(7, 99);" << endl;
+	std::cout << std::endl << "-> v1.resize(7, 99);" << std::endl;
 	v1.resize(7, 99);
-	cout << v1;
+	std::cout << v1;
 
-	cout << endl << "-> iterating v2 using iterators" << endl;
+	std::cout << std::endl << "-> iterating v2 using iterators" << std::endl;
 	auto it1 = v2.begin();
 	auto it2 = v2.end();
 	
 	while (it1 != it2)
-		cout << *(it1++) << ", ";
-	cout << endl;
+		std::cout << *(it1++) << ", ";
+	std::cout << std::endl;
 
-	cout << endl << "-> v2.insert(++++(v2.begin()), -99);" << endl;
+	std::cout << std::endl << "-> v2.insert(++++(v2.begin()), -99);" << std::endl;
 	v2.insert(++++(v2.begin()), -99);
-	cout << v2;
+	std::cout << v2;
 
-	cout << endl << "-> v2.insert(++++(v2.begin()), v1.begin(), ----(v1.end()));" << endl;
+	std::cout << std::endl << "-> v2.insert(++++(v2.begin()), v1.begin(), ----(v1.end()));" << std::endl;
 	v2.insert(++++(v2.begin()), v1.begin(), ----(v1.end()));
-	cout << v2;
+	std::cout << v2;
 	
-	cout << endl << "-> v2.erase(++++++(v2.begin()));" << endl;
+	std::cout << std::endl << "-> v2.erase(++++++(v2.begin()));" << std::endl;
 	v2.erase(++++++(v2.begin()));
-	cout << v2;
+	std::cout << v2;
 
-	cout << endl << "-> v2.erase(++++++v2.begin(), ------v2.end());" << endl;
+	std::cout << std::endl << "-> v2.erase(++++++v2.begin(), ------v2.end());" << std::endl;
 	v2.erase(++++++v2.begin(), ------v2.end());
-	cout << v2;
+	std::cout << v2;
 
-	cout << endl << "-> v1 = {2,4,6,8,12,3,4,4,5,6,6,7};" << endl;
+	std::cout << std::endl << "-> v1 = {2,4,6,8,12,3,4,4,5,6,6,7};" << std::endl;
 	v1 = {2,4,6,8,12,3,4,4,5,6,6,7};
-	cout << v1;
+	std::cout << v1;
 
-	cout << endl << "-> v1 = Vec<int>(++v2.cbegin(), --v2.cend());" << endl;
+	std::cout << std::endl << "-> v1 = Vec<int>(++v2.cbegin(), --v2.cend());" << std::endl;
 	v1 = Vec<int>(++v2.cbegin(), --v2.cend());
-	cout << v1;
+	std::cout << v1;
 
 	v2 = {0,1,2,3,4,5,6,7,8};
 	auto v1_beg = v1.begin();
@@ -135,65 +129,64 @@ int main()
 	auto v2_beg = v2.begin();
 	auto v2_end = v2.end();
 
-	cout << endl << "v2: " << v2;
+	std::cout << std::endl << "v2: " << v2;
 	
-	cout << endl << "v1_beg = v1.begin();"
-		<< endl << "v1_end = v1.end();"
-		<< endl << "v2_beg = v2.begin();"
-		<< endl << "v2_end = v2.end();"<< endl;
+	std::cout << std::endl << "v1_beg = v1.begin();"
+		<< std::endl << "v1_end = v1.end();"
+		<< std::endl << "v2_beg = v2.begin();"
+		<< std::endl << "v2_end = v2.end();"<< std::endl;
 	
-	cout << endl << "-> v2_beg[4]: " << v2_beg[4] << endl;
-	cout << "-> v2_end - v2_beg: " << v2_end - v2_beg << endl;
-	cout << "-> *(v2_beg + 2): " << *(v2_beg + 2) << endl;
-	cout << "-> *(v2_end - 2): " << *(v2_end - 2) << endl << endl;
+	std::cout << std::endl << "-> v2_beg[4]: " << v2_beg[4] << std::endl;
+	std::cout << "-> v2_end - v2_beg: " << v2_end - v2_beg << std::endl;
+	std::cout << "-> *(v2_beg + 2): " << *(v2_beg + 2) << std::endl;
+	std::cout << "-> *(v2_end - 2): " << *(v2_end - 2) << std::endl << std::endl;
 	
-	cout << "v2_beg < v2_end: ";
+	std::cout << "v2_beg < v2_end: ";
 	if (v2_beg < v2_end)
-		cout << "true" << endl;
+		std::cout << "true" << std::endl;
 	else
-		cout << "false" << endl;
+		std::cout << "false" << std::endl;
 
-	cout << "v2_beg > v2_end: ";
+	std::cout << "v2_beg > v2_end: ";
 	if (v2_beg > v2_end)
-		cout << "true" << endl;
+		std::cout << "true" << std::endl;
 	else
-		cout << "false" << endl;
+		std::cout << "false" << std::endl;
 
-	cout << "v2_beg < v1_end: ";
+	std::cout << "v2_beg < v1_end: ";
 	if (v2_beg < v1_end)
-		cout << "true" << endl;
+		std::cout << "true" << std::endl;
 	else
-		cout << "false" << endl;
+		std::cout << "false" << std::endl;
 
-	cout << endl;
+	std::cout << std::endl;
 
-	cout << "v2_beg <= v2_end: ";
+	std::cout << "v2_beg <= v2_end: ";
 	if (v2_beg <= v2_end)
-		cout << "true" << endl;
+		std::cout << "true" << std::endl;
 	else
-		cout << "false" << endl;
+		std::cout << "false" << std::endl;
 
-	cout << "v2_beg >= v2_end: ";
+	std::cout << "v2_beg >= v2_end: ";
 	if (v2_beg >= v2_end)
-		cout << "true" << endl;
+		std::cout << "true" << std::endl;
 	else
-		cout << "false" << endl;
+		std::cout << "false" << std::endl;
 
-	cout << "v2_beg <= v1_end: ";
+	std::cout << "v2_beg <= v1_end: ";
 	if (v2_beg <= v1_end)
-		cout << "true" << endl;
+		std::cout << "true" << std::endl;
 	else
-		cout << "false" << endl;
+		std::cout << "false" << std::endl;
 
-	cout << "v2_beg >= v2.begin(): ";
+	std::cout << "v2_beg >= v2.begin(): ";
 	if (v2_beg >= v2.begin())
-		cout << "true" << endl;
+		std::cout << "true" << std::endl;
 	else
-		cout << "false" << endl;
+		std::cout << "false" << std::endl;
 
-	cout << endl << "*(v2_beg += 6): " << *(v2_beg += 6) << endl;
-	cout << "*(v2_beg += 3): " << *(v2_beg -= 3) << endl;
-
+	std::cout << std::endl << "*(v2_beg += 6): " << *(v2_beg += 6) << std::endl;
+	std::cout << "*(v2_beg += 3): " << *(v2_beg -= 3) << std::endl;
 
 	return 0;
 }
